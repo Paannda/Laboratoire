ec3bcf202c9505a327ae329b838818ccc54e9baa
Paannda <simon.mera@gmail.com>
2016-09-27 18:52:25 -0400

    Initial Commit

---
+++ Laboratoire iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laboratoire iOS
+//
+//  Created by Simon Méra on 16-09-27.
+//  Copyright © 2016 ETS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
