49938518ca8213d862737e7b9618b9644552d0b3
Paannda <simon.mera@gmail.com>
2016-10-04 15:15:41 -0400

    Première ébauche d'interface

--- Laboratoire iOS/ViewController.swift
+++ Laboratoire iOS/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+  var ClubArray:NSArray = []
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let path = Bundle.main.path(forResource: "Club_List", ofType: "plist")
+    ClubArray = NSArray(contentsOfFile: path!)!
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +23,19 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBOutlet weak var appTableView: UITableView!
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return ClubArray.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell")!
+    
+    cell.textLabel?.text = "Test"
+    cell.detailTextLabel?.text = "Sous-titre #\(indexPath.row)"
+    cell.imageView?.image = UIImage(named: "ic_clubs/ic_ace.png")
+    return cell
+  }
 }
 
